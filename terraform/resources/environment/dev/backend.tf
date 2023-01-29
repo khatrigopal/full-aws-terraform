df444f0666b5000fc9a2b18061af3c7e85519ed9
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-01-29 13:06:42 +0530

    Create backend.tf

---
+++ terraform/resources/environment/dev/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+## Backend ###
+# S3
+###############
+
+terraform {
+  backend "s3" {
+    bucket = "khatrig-githubaction"
+    key = "dev-s3.tfstate"
+    region = "us-east-1"
+  }
+}
